0eb4c24cf028363706a06b333801deb3f5f70bcd
Arthur <arthur.oda@fatec.sp.gov.br>
2024-10-09 16:05:35 -0300

    images thumbnail

--- src/models/Image.js
+++ src/models/Image.js
@@ -1,10 +1,11 @@
 export default class Image {
-    constructor({filePath, name, xResolution, yResolution, size, blob}) {
+    constructor({filePath, name, xResolution, yResolution, size, blob, url}) {
         this.filePath = filePath;
         this.name = name;
         this.xResolution = xResolution;
         this.yResolution = yResolution;
         this.size = size;
         this.blob = blob;
+        this.url = url;
     }
 }
