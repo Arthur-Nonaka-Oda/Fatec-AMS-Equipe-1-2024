799327436faab359b169a27a823172f4f276de06
buja23 <jooj.soares.227@gmail.com>
2025-08-28 16:45:11 -0300

    "icones importados"

--- vue/src/main.js
+++ vue/src/main.js
@@ -6,6 +6,18 @@ Vue.config.productionTip = false
 
 Vue.prototype.$files = new Files();
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+// Ícones que você quer usar
+import { faUser, faLock, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+// Componente do Vue
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Adiciona os ícones à biblioteca
+library.add(faUser, faLock, faAngleUp)
+
+// Registra o componente globalmente
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 new Vue({
   render: h => h(App),
